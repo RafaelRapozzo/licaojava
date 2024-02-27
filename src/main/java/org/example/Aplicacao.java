033f34a0506e96394eeab502a6b691d0f123bbcd
Gabssil <gabriela.silva1532@etec.sp.gov.br>
2024-02-27 20:13:27 -0300

    licaojava

---
+++ src/main/java/org/example/Aplicacao.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Aplicacao {
+    public static void main(String args[]) {
+        Pessoa p1 = new Pessoa("Hugozinho");
+        Pessoa p2 = new Pessoa("Zezinho");
+        System.out.println(p1.nome);
+        System.out.println(p2.nome);
+    }
+}
---
+++ src/main/java/org/example/Pessoa.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Pessoa {
+    String nome;
+
+        public Pessoa(String nome) {
+            this.nome = nome;
+
+        }
+
+
+
+
+
+
+}
